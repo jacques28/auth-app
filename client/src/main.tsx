b9495030378262d9cf3af706408bfee2d1a48b1e
jacques28 <devjacques28i@gmail.com>
2023-07-19 11:53:09 -0400

    Fix: Routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// import App from './App';
+import './index.css';
+import { createBrowserRouter,RouterProvider,Route,Link } from "react-router-dom";
+import Home from './pages/Home';
+import NotFind from './pages/NotFind';
+
+
+const router = createBrowserRouter(
+  [
+   { path: "/", element: <Home /> },
+   { path: "*", element: <NotFind /> },
+ ]
+);
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   <RouterProvider router={router} />
+  </React.StrictMode>
+);
